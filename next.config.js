50844a1697b448d4145ca957779f3fe0bce2a76b
BrandonTruter <brandonleetruter@gmail.com>
2024-06-05 05:42:45 +0200

    setup deploy

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = '';
 let basePath = '';
-
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
   assetPrefix = `/${repo}/`;
   basePath = `/${repo}`;
 }
-
-module.exports = {
+const nextConfig = {
+  output: 'export',
   assetPrefix: assetPrefix,
   basePath: basePath,
   trailingSlash: true,
   reactStrictMode: true,
-	experimental: { appDir: true },
-};
+}
+ 
+module.exports = nextConfig
